f2b309760c987a232c57f03754f98e6746730429
sonya <1281855126@qq.com>
2017-11-22 18:16:45 +0800

    +node.js

---
+++ 项目实战—慕课网~~~~/6.Node.js/1.进击nodejs基础一/2.School/class.js
@@ -0,0 +1,16 @@
+var student = require("./student");
+var teacher = require("./teacher");
+
+function add(teacherName,students){
+	teacher.add(teacherName);
+	students.forEach(function(item,index){
+		student.add(item);
+	});
+
+	/*students.each(function(item,index){  
+		student.add(item);
+	});*/
+
+}
+
+exports.add = add;
---
+++ 项目实战—慕课网~~~~/6.Node.js/1.进击nodejs基础一/3.http/callback.js
@@ -0,0 +1,13 @@
+function learn(something){
+	console.log(something);
+}
+function we(callback,something){
+	something += "is cool";
+	callback(something);
+}
+
+we(learn,"node ");
+
+we(function (some){
+	console.log(some);
+},"sonya");
